4ad37938e985622bf90b47c23ac481a06f8d76da
matiasgoffi <matiasgoffi@hotmail.com>
2022-10-03 16:59:51 -0300

    cambio nombre a carpeta de build a doc

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './styles.css'
 
 
 ReactDOM.createRoot( document.getElementById('root')).render(
-     <React.StrictMode>
+    //  <React.StrictMode>
        <GifExperApp />
-     </React.StrictMode>
+     // </React.StrictMode>
 );
